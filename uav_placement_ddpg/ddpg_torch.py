db0c1b8ca3ea0e8633c9d94f8191e0d08d6d7992
Chanakya <ca10@iitbbs.ac.in>
2021-03-25 13:52:31 +0530

    Completed the DDPG Code
    Signed-off-by: Chanakya <ca10@iitbbs.ac.in>

--- uav_placement_ddpg/ddpg_torch.py
+++ uav_placement_ddpg/ddpg_torch.py
@@ -41,6 +41,7 @@ class ReplayBuffer:
         self.action_memory[index] = action
         self.reward_memory[index] = reward
         self.new_state_memory[index] = state_
+        self.mem_cntr += 1
 
     def sample_buffer(self, batch_size):
         max_mem = min(self.mem_cntr, self.mem_size)
@@ -139,13 +140,78 @@ class Agent():
 
         self.update_network_parameters(tau=1)
 
+    def choose_action(self, observation):
+        self.actor.eval()
+        observation = T.tensor(observation, dtype=T.float).to(self.actor.device)
+        mu = self.actor(observation).to(self.actor.device)
+        mu_prime = mu + T.Tensor(self.noise(),
+                                 dtype=T.float).to(self.actor.device)
+        self.actor.train()
+        return mu_prime.cpu().detach.numpy()
 
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+        states, actions, rewards, next_states = \
+            self.memory.sample_buffer(self.batch_size)
+        rewards = T.tensor(rewards, dtype=T.float).to(self.critic.device)
+        next_states = T.tensor(next_states, dtype=T.float).to(self.critic.device)
+        actions = T.tensor(actions, dtype=T.float).to(self.critic.device)
+        states = T.tensor(states, dtype=T.float).to(self.critic.device)
 
+        self.target_actor.eval()
+        self.target_critic.eval()
+        self.critic.eval()
 
+        target_actions = self.target_actor.forward(next_states)
+        critic_value_ = self.target_critic.forward(next_states, target_actions)
+        critic_value = self.target_critic.forward(states, actions)
 
+        target = []
+        for j in range(self.batch_size):
+            target.append(rewards[j] + self.gamma*critic_value_[j])
+        target = T.tensor(target).to(self.critic.device)
+        target = target.view(self.batch_size, 1)
 
+        self.critic.train()
+        self.critic.optimizer.zero_grad()
+        critic_loss = F.mse_loss(target, critic_value)
+        critic_loss.backward()
+        self.critic.optimizer.step()
 
+        self.critic.eval()
+        self.actor.optimizer.zero_grad()
+        mu = self.actor.forward(states)
+        self.actor.train()
+        actor_loss = -self.critic.forward(states, mu)
+        actor_loss = T.mean(actor_loss)
+        actor_loss.backward()
+        self.actor.optimizer.step()
 
+        self.update_network_parameters()
 
+    def update_network_parameters(self, tau=None):
+        if tau is None:
+            tau = self.tau
 
+        actor_params = self.actor.named_parameters()
+        critic_params = self.critic.named_parameters()
+        target_actor_params = self.target_actor.named_parameters()
+        target_critic_params = self.target_critic.named_parameters()
 
+        critic_state_dict = dict(critic_params)
+        actor_state_dict = dict(actor_params)
+        target_critic_dict = dict(target_critic_params)
+        target_actor_dict = dict(target_actor_params)
+
+        for name in critic_state_dict:
+            critic_state_dict[name] = tau * critic_state_dict[name].clone() + \
+                                      (1 - tau) * \
+                                      target_critic_dict[name].clone()
+        self.target_critic.load_state_dict(critic_state_dict)
+
+        for name in actor_state_dict:
+            actor_state_dict[name] = tau * actor_state_dict[name].clone() + \
+                                     (1 - tau) * \
+                                     target_actor_dict[name].clone()
+        self.target_actor.load_state_dict(actor_state_dict)
